a966702c785eb1db54d5fa4a786936b614184462
Theta <DragosDragos2476@gmail.com>
2022-12-23 20:46:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ def PrintVer():
 if __name__ == "__main__":
     from updater import checkUpdates
     resp = checkUpdates()
+    print(f"This is version {getVersion()}!\n")
     if resp == True:print("Update installed.\nPress any key to exit.");input()
     else:print("No new updates available.\nPress any key to exit.");input()
